adb30c7c6c585e5ae8c66c0790d3c05667ae41e9
victor08 <yourjob@list.ru>
2015-03-09 17:12:40 +0300

    initial set up

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+/**
+ * Plugins
+ */
+var
+    gulp            = require('gulp')
+    , plumber       = require('gulp-plumber')
+    , less          = require('gulp-less')
+    , sourcemaps    = require('gulp-sourcemaps')
+    , _             = require('lodash')
+    , fs            = require('fs')
+    ;
+
+
+/**
+ * Paths
+ */
+var
+      public_css    = './app/assets/css'
+    , resources     = './resources'
+    , vendor        = './resources/vendor'
+    , public_vendor = './app/assets/vendor'
+    ;
+
+gulp.task('default', function () {
+    gulp.watch(assets + '/less/**/*.less', ['less']);
+});
+
+
+/**
+ * Tasks
+ */
+gulp.task('default', function () {
+    gulp.watch(resources + '/less/**/*.less', ['less']);
+});
+gulp.task('less', function () {
+    gulp.src(resources + '/less/index.less')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(less({compress: true, sourceMap: true}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(public_css));
+});
+
+gulp.task('public', function () {
+    var config = require('./bower.json')
+        , bowerrc = JSON.parse(fs.readFileSync('./.bowerrc'));
+
+    _.each(config.public, function (includes, name) {
+        _.each(includes, function (path) {
+            gulp.src('./' + bowerrc.directory + '/' + name + '/' + path)
+                .pipe(gulp.dest(public_vendor + '/' + name));
+        });
+    });
+});
